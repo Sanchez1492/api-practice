836611df03919f1c9a3fbeb42c972c3eac867e95
Sebastian Sanchez <dev.sebastian.sanchez@gmail.com>
2023-03-02 16:00:20 -0300

    Changed JS file with async functions and fixed the reload button so it does not reload the entire page

--- cat-api/main.js
+++ cat-api/main.js
@@ -1,10 +1,23 @@
 console.log('Hello world!');
 
-const URL = 'https://api.thecatapi.com/v1/images/search'
+const API_URL = 'https://api.thecatapi.com/v1/images/search';
 
-fetch(URL)
-    .then(res => res.json())
-    .then(data => {
-        const img = document.querySelector('img')
-        img.src = data[0].url
-    })
+//Ejercicio hecho con fetch
+
+// fetch(API_URL)
+//     .then(res => res.json())
+//     .then(data => {
+//         const img = document.querySelector('img')
+//         img.src = data[0].url
+//     })
+
+//Ejercicio hecho con async y await
+
+async function reload() {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    const img = document.querySelector('img');
+    img.src = data[0].url;
+}
+
+reload();
