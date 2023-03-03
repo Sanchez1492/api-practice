2cab3bf10d663a09fd72ecbfe556bb86f7105bee
Sebastian Sanchez <dev.sebastian.sanchez@gmail.com>
2023-03-03 07:26:42 -0300

    Designed the DOM for the course project

--- cat-api/main.js
+++ cat-api/main.js
@@ -1,6 +1,6 @@
 console.log('Hello world!');
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search';
+const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3&api-key=live_XSykOSHNW6kY0YL03SIvUIY9NWcefmN4hEHfuEqWQ9sMs8c4K7rQoBCCFWyKmquI';
 
 //Ejercicio hecho con fetch
 
@@ -16,8 +16,14 @@ const API_URL = 'https://api.thecatapi.com/v1/images/search';
 async function reload() {
     const response = await fetch(API_URL);
     const data = await response.json();
-    const img = document.querySelector('img');
-    img.src = data[0].url;
+    const picture1 = document.querySelector('#picture1');
+    picture1.src = data[0].url;
+
+    const picture2 = document.querySelector('#picture2');
+    picture2.src = data[1].url;
+
+    const picture3 = document.querySelector('#picture3');
+    picture3.src = data[2].url;
 }
 
 reload();
